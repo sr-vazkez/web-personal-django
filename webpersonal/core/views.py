bbd288c7252b44da1a9393bed85b3bff0ff904cf
Paul Vazquez Cervantes <paulvazk@protonmail.com>
2021-06-17 14:26:19 -0500

    agregadno rutas

--- webpersonal/core/views.py
+++ webpersonal/core/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render, HttpResponse, html_base
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+html_base = """
+<h1>Mi web personal</h1>
+<ul>
+     <li><a href="/">Portada</a></li>
+     <li><a href="/about/">Acerca de</a></li>
+     <li><a href="/portafolio/">portafolio</a></li>
+     <li><a href="/contact/">Contacto</a></li>
+</ul>
+"""
 
 
 def home(request):
--- webpersonal/webpersonal/urls.py
+++ webpersonal/webpersonal/urls.py
@@ -17,6 +17,9 @@ from django.contrib import admin
 from django.urls import path
 from core import views
 urlpatterns = [
-    path('home', views.home, name="home"),
+    path('', views.home, name="home"),
+    path('about', views.about, name="about"),
+    path('portafolio', views.portafolio, name="portafolio"),
+    path('contact', views.contact, name="contact"),
     path('admin/', admin.site.urls),
 ]
