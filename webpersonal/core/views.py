ebaa453403256cee925fb32f7906b1cf7e387299
Paul Vazquez Cervantes <paulvazk@protonmail.com>
2021-06-17 14:21:02 -0500

    agregando rutas

--- webpersonal/core/views.py
+++ webpersonal/core/views.py
@@ -1,7 +1,31 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, html_base
 
 # Create your views here.
 
 
 def home(request):
-    return HttpResponse("<h1>Prueba</h1><h2>Portada</h2>")
+    return HttpResponse(html_base + """
+                         <h2>Inicio</h2>
+                         <p>Texto chingon</p>
+                         """)
+
+
+def about(request):
+    return HttpResponse(html_base + """
+                         <h2>Acerca de Mi</h2>
+                         <p>Texto chingon</p>
+                         """)
+
+
+def portafolio(request):
+    return HttpResponse(html_base + """
+                         <h2>Portafolio</h2>
+                         <p>Texto chingon</p>
+                         """)
+
+
+def contact(request):
+    return HttpResponse(html_base + """
+                         <h2>Contactame</h2>
+                         <p>Texto chingon</p>
+                         """)
