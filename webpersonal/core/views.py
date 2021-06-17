955b51d2a2ffbcd6f4419e3e9a2f8e1542ea8a4a
Paul Vazquez Cervantes <paulvazk@protonmail.com>
2021-06-17 14:43:35 -0500

    quitando redundancia html

--- webpersonal/core/views.py
+++ webpersonal/core/views.py
@@ -1,15 +1,6 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
-html_base = """
-<h1>Mi web personal</h1>
-<ul>
-     <li><a href="/">Portada</a></li>
-     <li><a href="/about/">Acerca de</a></li>
-     <li><a href="/portafolio/">portafolio</a></li>
-     <li><a href="/contact/">Contacto</a></li>
-</ul>
-"""
 
 
 def home(request):
@@ -17,12 +8,12 @@ def home(request):
 
 
 def about(request):
-    return render(request, "core/home.html")
+    return render(request, "core/about.html")
 
 
 def portafolio(request):
-    return render(request, "core/home.html")
+    return render(request, "core/portafolio.html")
 
 
 def contact(request):
-    return render(request, "core/home.html")
+    return render(request, "core/contact.html")
