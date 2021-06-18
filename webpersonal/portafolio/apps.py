8512ca3e633d7be156149308280acd2e6314a165
Paul Vazquez Cervantes <paulvazk@protonmail.com>
2021-06-17 21:58:16 -0500

    cambiado nombre del proyecto

--- webpersonal/portafolio/apps.py
+++ webpersonal/portafolio/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class PortafolioConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'portafolio'
+    verbose_name = 'Portafolio'
