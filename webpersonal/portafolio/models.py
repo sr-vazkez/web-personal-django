94843c26f9de0666d126aa34754e630231037635
Paul Vazquez Cervantes <paulvazk@protonmail.com>
2021-06-17 21:41:36 -0500

    agregando bien el modelo

--- webpersonal/portafolio/models.py
+++ webpersonal/portafolio/models.py
@@ -4,8 +4,8 @@ from django.db import models
 
 
 class Project(models.Model):
-    title = models.CharField()
+    title = models.CharField(max_length=200)
     description = models.TextField()
-    image = models.models.ImageField()
+    image = models.ImageField()
     created = models.DateField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
