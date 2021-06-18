bdc611820dc06a25df42e68fdb0e867bc0b40809
Paul Vazquez Cervantes <paulvazk@protonmail.com>
2021-06-18 00:00:16 -0500

    sirviendo ficheros en desarrollo

--- webpersonal/webpersonal/urls.py
+++ webpersonal/webpersonal/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from core import views
+
+from django.conf import settings
+
 urlpatterns = [
     path('', views.home, name="home"),
     path('about', views.about, name="about"),
@@ -23,3 +26,8 @@ urlpatterns = [
     path('contact', views.contact, name="contact"),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
