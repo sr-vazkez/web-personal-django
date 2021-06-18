777aed5c31b21515db9938c6d4c132251b24fa48
Paul Vazquez Cervantes <paulvazk@protonmail.com>
2021-06-18 00:31:38 -0500

    Finalizando la web personal

--- webpersonal/portafolio/models.py
+++ webpersonal/portafolio/models.py
@@ -7,6 +7,7 @@ class Project(models.Model):
     title = models.CharField(max_length=200, verbose_name="Titulo")
     description = models.TextField(verbose_name="Descripcion")
     image = models.ImageField(verbose_name="Imagen", upload_to="projects")
+    link = models.URLField(verbose_name="Direccion Web", null=True, blank=True)
     created = models.DateField(
         auto_now_add=True, verbose_name="Fecha de Creacion")
     updated = models.DateTimeField(
