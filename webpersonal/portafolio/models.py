c4e9702a496dfc9cb88eaf5284009e1c7e23ce21
Paul Vazquez Cervantes <paulvazk@protonmail.com>
2021-06-17 22:01:37 -0500

    traduciendo

--- webpersonal/portafolio/models.py
+++ webpersonal/portafolio/models.py
@@ -9,3 +9,8 @@ class Project(models.Model):
     image = models.ImageField()
     created = models.DateField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Proyecto"
+        verbose_name_plural = "Proyectos"
+        ordering = ["-created"]
